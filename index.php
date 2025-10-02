1596b63cab14899cd119d75b646c96bb795efcba
tong1104 <j22038181@student.newinti.edu.my>
2025-10-02 14:34:09 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $content = "
-<h1>Hello, I'm Kam Yen Tong 👋</h1>
+<h1>Hi, I'm Kam Yen Tong 👋</h1>
 <p>Data Science major passionate about machine learning, data analysis, and cloud technologies.  
 I enjoy turning raw data into meaningful insights and building intelligent solutions.</p>
 ";
